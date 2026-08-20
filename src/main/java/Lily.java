0653f26616a455912e30066fac499ccdd988138f
aseera <aseerajannath@gmail.com>
2026-08-21 01:46:04 +0800

    Edit chatbot name and add welcome message

---
+++ src/main/java/Lily.java
@@ -0,0 +1,18 @@
+public class Lily {
+    public static void main(String[] args) {
+        String banner = " _     _ _       \n"
+                + "| |   (_) |      \n"
+                + "| |    _| |_   _ \n"
+                + "| |   | | | | | |\n"
+                + "| |___| | | |_| |\n"
+                + "\\_____/_|\\__, |  \n"
+                + "          __/ |  \n"
+                + "         |___/   \n";
+        String openingMessage = "Hey there! I'm Lily.\nWhat would you like to do today?";
+        String closingMessage = "Bye! See you soon :)";
+        System.out.println(banner);
+        System.out.println(openingMessage);
+        System.out.println();
+        System.out.println(closingMessage);
+    }
+}
