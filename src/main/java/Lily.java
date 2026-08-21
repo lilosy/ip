8aaf376f78050e39ddc8a68d98873fff554ab548
aseera <aseerajannath@gmail.com>
2026-08-21 21:26:23 +0800

    Add feature to add text and display it back when requested

--- src/main/java/Lily.java
+++ src/main/java/Lily.java
@@ -13,6 +13,8 @@ public class Lily {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         String openingMessage = "Hey there! I'm Lily.\nWhat would you like to do today?";
         String closingMessage = "Bye! See you soon :)";
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println(banner);
         System.out.println(openingMessage);
@@ -21,10 +23,19 @@ public class Lily {
             String userInput = myObj.nextLine();  // Read user input
             if (userInput.equals("bye")) {
                 break;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(String.format("%d. %s", i + 1, tasks[i]));
+                }
+            } else {
+                tasks[taskCount] = userInput;
+                taskCount++;
+
+                System.out.println("---------------------");
+                System.out.println("added: " + userInput);
+                System.out.println("---------------------");
             }
-            System.out.println("---------------------");
-            System.out.println(userInput);
-            System.out.println("---------------------");
+
         }
 
         System.out.println();
