380bca2b71ae03b1ae474e71ec6e8db767efb726
aseera <aseerajannath@gmail.com>
2026-08-21 20:15:24 +0800

    Add feature to echo commands by users

--- src/main/java/Lily.java
+++ src/main/java/Lily.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lily {
     public static void main(String[] args) {
         String banner = " _     _ _       \n"
@@ -8,10 +10,23 @@ public class Lily {
                 + "\\_____/_|\\__, |  \n"
                 + "          __/ |  \n"
                 + "         |___/   \n";
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         String openingMessage = "Hey there! I'm Lily.\nWhat would you like to do today?";
         String closingMessage = "Bye! See you soon :)";
+
         System.out.println(banner);
         System.out.println(openingMessage);
+
+        while (true) {
+            String userInput = myObj.nextLine();  // Read user input
+            if (userInput.equals("bye")) {
+                break;
+            }
+            System.out.println("---------------------");
+            System.out.println(userInput);
+            System.out.println("---------------------");
+        }
+
         System.out.println();
         System.out.println(closingMessage);
     }
